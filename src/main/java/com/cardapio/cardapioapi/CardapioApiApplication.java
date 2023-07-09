9ec40707e5d90ea91d34b8f3f36c7895bd65a4cb
Gustavo Ferreira <gustavo.jesus@proton.me>
2023-07-09 17:52:33 -0300

    Initial Commit

---
+++ src/main/java/com/cardapio/cardapioapi/CardapioApiApplication.java
@@ -0,0 +1,13 @@
+package com.cardapio.cardapioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardapio/cardapioapi/CardapioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardapio.cardapioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
